833071065b0da2b52b466adc363d61cd02308ea1
syaifulfahmi <52196122+syaifulfahmi@users.noreply.github.com>
2020-10-08 12:24:19 +0700

    Add files via upload

---
+++ tugas_6b.php
@@ -0,0 +1,13 @@
+<?php
+
+	$jambu = 6*5;
+	$kardus = 2;
+	$harga_kardus = 2000;
+	$harga_jambu = 15000 ;
+	$jual = ($jambu*$harga_jambu)+($kardus*$harga_kardus);
+
+	echo "======================================= <br>";
+	echo "Harga Jambu = Rp. ".$harga_jambu," / Kg<br>","Harga Kardus = ".$harga_kardus," /pcs <br>","Total Penjualan (Dus dan Jambu) = Rp. ".$jual;
+	echo "<br>=======================================";
+
+?>
